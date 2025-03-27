ccbb766dbbe1b75e302aa4a0638f001fda8bce90
jhefferson <jhefferson.w.dev@gmail.com>
2025-03-27 15:57:13 -0300

    feat: renomear formulário inicial para Frm_Intro e implementar transição automática de tela
    - Renomeado o formulário inicial "Form1" para "Frm_Intro".
    - Implementada a transição automática da tela de Frm_Intro para Frm_Principal.
    - Atualizada a imagem de fundo para "banner_DPFacil.png".
    - Alterados os ícones do aplicativo e do formulário para "icon_DPFacil.ico".
    - Configurado o sistema para fechar após o último formulário ser fechado.

---
+++ Frm_Intro.Designer.vb
@@ -0,0 +1,44 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Frm_Intro
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_Intro))
+        Me.SuspendLayout()
+        '
+        'form_Intro_DPFacil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.DP_Fácil.My.Resources.Resources.banner_DPFacil
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(634, 354)
+        Me.ControlBox = False
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "form_Intro_DPFacil"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ Frm_Intro.vb
@@ -0,0 +1,29 @@
+﻿Public Class Frm_Intro
+
+    ' Timer para controlar o tempo de exibição do FormIntro
+    Private WithEvents Tmr_Intro As New Timer()
+
+    ' Quando o formulário for carregado, inicializa o timer
+    Private Sub FormIntro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Configura o intervalo do timer (5 segundos = 5000 milissegundos)
+        Tmr_Intro.Interval = 3000 ' 5 segundos
+        Tmr_Intro.Start() ' Inicia o timer
+
+    End Sub
+
+    ' Evento do timer que ocorre a cada intervalo
+    Private Sub introTimer_Tick(sender As Object, e As EventArgs) Handles Tmr_Intro.Tick
+
+        ' Para o timer para evitar múltiplos ticks
+        Tmr_Intro.Stop()
+
+        ' Abre o FormPrincipal e fecha o FormIntro
+        'Dim frm_Principal As New form_Principal()
+        'frm_Principal.Show()
+
+        Me.Close()
+
+    End Sub
+
+End Class
